8af829b833f4fa1a99e2b6c270d6fb8badeded5e
Raul Zuniga <rzuniga64@gmail.com>
2013-08-14 00:52:15 -0500

    Add udemy recursion examples

---
+++ udemy/src/recursion/factorial.py
@@ -0,0 +1,12 @@
+def factorial(number):
+    product = 1
+    for i in range(1, number+1):
+        product *= i
+    return product
+
+def main():
+    print("Enter a number: ")
+    num = int(input())
+    print(str(num) + "! equals " + str(factorial(num)))
+    
+main()
