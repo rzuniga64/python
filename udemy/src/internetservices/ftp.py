f156a2618b85b99dfe3ed44f938c2c281c195f3d
Raul Zuniga <rzuniga64@gmail.com>
2013-09-06 00:05:37 -0500

    Add ftp script

---
+++ udemy/src/internetservices/ftp.py
@@ -0,0 +1,23 @@
+import ftplib
+import sys
+
+def getdir():
+    connect = ftplib.FTP("www.ualr.edu")
+    connect.login("facstaff\mmmcmillan", "meredith26")
+    data = list()
+    connect.dir(data.append)
+    connect.quit()
+    for line in data:
+        print(line)
+        
+def getfile():
+    filename = sys.argv[1]
+    connect = ftplib.FTP("www.ualr.edu")
+    connect.login("facstaff\mmmcmillan", "meredith26")
+    connect.retrlines("RETR " + filename)
+    connect.quit()
+    
+def main():
+    getdir();
+    
+main()
