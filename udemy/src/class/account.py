f0f1da57a0185a65e8246bb62633ca0d34b4c844
Raul Zuniga <rzuniga64@gmail.com>
2013-10-30 00:30:37 -0500

    Minor edits

--- udemy/src/class/account.py
+++ udemy/src/class/account.py
@@ -1,33 +1,34 @@
 class Account:
-   def __init__(self, acctNumber, balance):
-      self.acctNumber = acctNumber
-      self.balance = balance
+    def __init__(self, acct_number, balance):
+        self.acct_number = acct_number
+        self.balance = balance
 
-   def __str__(self):
-      return "Account number: " + str(self.acctNumber) + "\n" + \
-             "Balance: " + str(self.balance)
+    def __str__(self):
+        return "Account number: " + str(self.acct_number) + "\n" + \
+            "Balance: " + str(self.balance)
+
 
 class Checking(Account):
-   def __init__(self, acctNumber, balance, fee):
-      Account.__init__(self, acctNumber, balance)
-      self.fee = fee
+    def __init__(self, acct_number, balance, fee):
+        Account.__init__(self, acct_number, balance)
+        self.fee = fee
 
-   def __str__(self):
-      retStr = "Account type: Checking\n";
-      retStr += Account.__str__(self)
-      return retStr
+    def __str__(self):
+        ret_str = "Account type: Checking\n"
+        ret_str += Account.__str__(self)
+        return ret_str
 
-   def getFee(self):
-      return self.fee
+    def get_fee(self):
+        return self.fee
 
-   def deposit(self, amount):
-      self.balance += amount
+    def deposit(self, amount):
+        self.balance += amount
 
-   def withdraw(self, amount):
-      if amount > self.balance:
-         print("Insufficient funds.")
-      else:
-         self.balance = self.balance - amount - self.fee
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds.")
+        else:
+            self.balance = self.balance - amount - self.fee
 
 ca = Checking("123", 500, .50)
 print(ca)
