2626248582e7b956d6988bab428f21074f7ce119
Raul Zuniga <rzuniga64@gmail.com>
2013-10-30 00:45:20 -0500

    Minor edits

--- udemy/src/iterators/dictionaryiter.py
+++ udemy/src/iterators/dictionaryiter.py
@@ -1,5 +1,5 @@
 def main():
-    grades = {'Cynthia':88, 'David':77, 'Clayton':99}
+    grades = {'Cynthia': 88, 'David': 77, 'Clayton': 99}
     for key in grades.keys():
         print(key, grades[key])
         
