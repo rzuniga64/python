6e3c01514ef6d2998b79e35e8fa409f79aeade32
Raul Zuniga <rzuniga64@gmail.com>
2013-10-30 00:39:53 -0500

    Minor edits

--- udemy/src/function/chap12ex2.py
+++ udemy/src/function/chap12ex2.py
@@ -1,17 +1,20 @@
 # Let's create an  acronym: in my humble opinion
 #IMHO
 
+
 def first(word):
-   return word[0]
+    return word[0]
+
 
 def acronym(words):
-   acro = ''
-   acro = acro.join(list(map(first, words))).upper()
-   return acro
+    acro = ''
+    acro = acro.join(list(map(first, words))).upper()
+    return acro
+
 
 def main():
-    words = ['in','my','humble','opinion']
-    acro = acronym(words)
-    print(acro)
+        words = ['in' ,'my', 'humble', 'opinion']
+        acro = acronym(words)
+        print(acro)
     
 main()
