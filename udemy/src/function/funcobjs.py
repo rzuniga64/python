0478d0334f9d2aa0347be3e8d0f26f2365fe1712
Raul Zuniga <rzuniga64@gmail.com>
2013-10-30 00:41:51 -0500

    Minor edits

--- udemy/src/function/funcobjs.py
+++ udemy/src/function/funcobjs.py
@@ -1,18 +1,19 @@
 def square(number):
-   return number * number
+    return number * number
+
 
 def main():
     num = 2
-    sqnum = square(num)
+    sq_num = square(num)
     # a function is just an object and can 
     # be assigned to any variable
-    sqnumber = square
-    sqnum = sqnumber(2)
-    print(sqnum)
+    sq_number = square
+    sq_num = sq_number(2)
+    print(sq_num)
 
     #map higher-order function
-    numbers = [1,2,3,4]
-    numberssq = list(map(square, numbers))
-    print(numberssq)
+    numbers = [1, 2, 3, 4]
+    numbers_sq = list(map(square, numbers))
+    print(numbers_sq)
     
 main()
