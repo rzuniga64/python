ffbbbe4d242a9262c4c94ef0fbc7b065ab736874
Raul Zuniga <rzuniga64@gmail.com>
2013-10-30 00:42:36 -0500

    Minor edits

--- udemy/src/internetservices/ftp.py
+++ udemy/src/internetservices/ftp.py
@@ -1,6 +1,7 @@
 import ftplib
 import sys
 
+
 def getdir():
     connect = ftplib.FTP("www.ualr.edu")
     connect.login("facstaff\mmmcmillan", "meredith26")
@@ -9,15 +10,17 @@ def getdir():
     connect.quit()
     for line in data:
         print(line)
-        
+
+
 def getfile():
     filename = sys.argv[1]
     connect = ftplib.FTP("www.ualr.edu")
     connect.login("facstaff\mmmcmillan", "meredith26")
     connect.retrlines("RETR " + filename)
     connect.quit()
-    
+
+
 def main():
-    getdir();
+    getdir()
     
 main()
