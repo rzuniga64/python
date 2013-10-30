445b786d7629951d4c9d8277b714ec68a64e50d1
Raul Zuniga <rzuniga64@gmail.com>
2013-10-30 00:24:48 -0500

    Minor edits

--- udemy/src/datastructures/numwords.py
+++ udemy/src/datastructures/numwords.py
@@ -6,10 +6,10 @@ print("Sentence in sorted order:\n")
 print(words)
 numWords = {}
 for i in range(0, len(words)):
-   if words[i] in numWords:
-      numWords[words[i]] += 1
-   else:
-      numWords[words[i]] = 1
+    if words[i] in numWords:
+        numWords[words[i]] += 1
+    else:
+        numWords[words[i]] = 1
 print("Word list and count: \n")
 for key in numWords.keys():
    print(key, numWords[key])
