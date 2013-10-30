49205147ba3fcf0848535159ac38a9d1edb40b94
Raul Zuniga <rzuniga64@gmail.com>
2013-10-30 00:37:42 -0500

    Minor edits

--- udemy/src/exceptionhandling/finally.py
+++ udemy/src/exceptionhandling/finally.py
@@ -1,13 +1,11 @@
 try:
-   print("Enter a file name: ")
-   name = input()
-   file = open(name, 'w')
-   display(file)
+    print("Enter a file name: ")
+    name = input()
+    file = open(name, 'w')
 except:
-   print("Error with file.")
-   print("Enter a file name: ")
-   name = input()
-   file = open(name, 'w')
-   display(file)
+    print("Error with file.")
+    print("Enter a file name: ")
+    name = input()
+    file = open(name, 'w')
 finally:
-   file.close()
+    file.close()
