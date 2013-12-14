68e4496008f4004f08314a86fe35ac3fa2d0fd78
Raul Zuniga <rzuniga64@gmail.com>
2013-12-13 19:22:20 -0600

    Add ITSE 1359 Chapter 8 file

--- itse1359/src/examples/ch8_files_and_directories/exceptions_in_os.py
+++ itse1359/src/examples/ch8_files_and_directories/exceptions_in_os.py
@@ -1,5 +1,5 @@
 import os.path
-
+import time
 
 # Split a path completely with a recursive function
 # Notice the two uses of single-element tuples, which
@@ -11,6 +11,39 @@ def split_fully(path):
         return parent_path,
     else:
         return split_fully(parent_path) + (name, )
+
+# This function loops over the list returned by os.listdir and
+# calls os.path.join on each entry to construct the full path
+# before printing it.
+
+
+def print_dir(dir_path):
+        for name in os.listdir(dir_path):
+            print(os.path.join(dir_path, name))
+
+# os.path.dir can be combined with os.listdir to process
+# subdirectories recursively.  You can list the contents
+# of a directory, its subdirectories, their subdirectories
+# and so on.  When the function finds a subdirectory, it
+# calls itself to list the contents of that subdirectory
+
+
+def print_tree(dir_path):
+    for name in os.listdir(dir_path):
+        full_path = os.path.join(dir_path, name)
+        print(full_path)
+        if os.path.isdir(full_path):
+            print_tree(full_path)
+
+
+# modify print_dir to print the contents of a directory,
+# including the size and modification time of each file.
+def print_dir_info(dir_path):
+    for name in os.listdir(dir_path):
+        full_path = os.path.join(dir_path, name)
+        file_size = os.getsize(full_path)
+        mod_time = time.ctime(os.path.getmtime(full_path))
+        print("%%-32s: %8d bytes, modified %s" % (name, file_size, mod_time))
 
 
 def main():
@@ -32,7 +65,42 @@ def main():
     # extension with os.path.splitext
     parts = os.path.splitext('image.jpg')
     extension = parts[1]
+    extension2 = os.path.splitext("image.jpg")[1]
 
-    print()
+    # os.path.normpath normalizes or "cleans up" a path:
+    norm_path = os.path.normpath(r"C\Program Files\Perl\..\Python30")
+
+    # os.path.abspath, which converts a relative path (a path relative
+    # to the current directory) to an absolute path (a path starting
+    # at the root of the drive or file system.
+    abs_path = os.path.abspath('file1.txt')
+
+    # If you want to know whether a path actually does exist, use
+    # os.path.exists
+    path_exists = os.path.exists("C:\\Users\\Roy\\PycharmProjects\\python")
+
+    # Get a list of entries in a directory
+    dir = os.listdir("C:\\Users\\Roy\\PycharmProjects\\python")
+
+    print_dir("C:\\Users\\Roy\\PycharmProjects\\python")
+
+    # This produces a case-sensitive alphabetical sort
+    sorted_list = sorted(os.listdir("C:\\Users\\Roy\\PycharmProjects\\python"))
+
+    # Determine if a path refers to a file using os.path.isfile
+    is_path = os.path.isfile("C:\\Users\\Roy\\PycharmProjects\\python\\oracle-driver-license.txt")
+
+    # Determine if a path refers to a directory using os.path.isdir
+    is_dir = os.path.isdir("C:\\Users\\Roy\\PycharmProjects\\python")
+
+    # print out the contents of directory
+    print_tree("C:\\Users\\Roy\\PycharmProjects\\python")
+
+    # Output when your Python directory was last modified
+    mod_time = os.path.getmtime("C:\Python33")
+    print(time.ctime(mod_time))
+
+    print_dir_info("C:\Python33")
+
 
 main()
