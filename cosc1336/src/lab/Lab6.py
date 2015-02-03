9243a025c276989950d0e534d674eaeb9c7dc4b5
Raul Zuniga <rzuniga64@gmail.com>
2015-02-03 01:02:46 -0600

    cleaned up file

--- cosc1336/src/lab/Lab6.py
+++ cosc1336/src/lab/Lab6.py
@@ -1,7 +1,8 @@
 import random
 
+
 def genSuit():
-    suit = random.randint( 1, 4 )
+    suit = random.randint(1, 4)
     if suit == 1:
         return "Clubs"
     if suit == 2:
@@ -12,8 +13,9 @@ def genSuit():
         return "Spades"
     return "Whoops!"
 
+
 def genRank():
-    rank = random.randint( 1, 13 )
+    rank = random.randint(1, 13)
     if rank == 1:
         return "Ace"
     if rank == 11:
@@ -22,51 +24,56 @@ def genRank():
         return "Queen"
     if rank == 13:
         return "King"
-    return str( rank )
+    return str(rank)
+
 
 def drawCard():
     return genRank() + " of " + genSuit()
 
+
 def test():
     n = 0
     while n < 10:
-        print( drawCard() )
+        print(drawCard())
         n += 1
 
-def value( card ):
-    position = card.find( " " )
+
+def value(card):
+    position = card.find(" ")
     rank = card[0:position]
     # print( rank )
     if rank == "Ace":
         return 11
     if rank == "King" or rank == "Queen" or rank == "Jack":
         return 10
-    return int( rank )
+    return int(rank)
+
 
 def hand():
     card1 = drawCard()
     card2 = drawCard()
-    print( card1, ", ", card2, sep="" )
-    return (value(card1),value(card2))
+    print(card1, ", ", card2, sep="")
+    return (value(card1), value(card2))
+
 
 def hitme():
     card = value(drawCard())
     print("The draw card is", card)
     return card
 
+
 def theGame():
-    card1,card2 = hand()
+    card1, card2 = hand()
     print(card1, card2)
     total = card1 + card2
     if total <= 16:
         print("Hit me!")
-        total = total + hitme()
+        total += hitme()
     elif total > 16 and total < 21:
         print("Don't hit me")
     elif total == 21:
         print("Blackjack!")
 
-        
     print("The total is ", total)
     if total > 21:
         print("You've lost!")
