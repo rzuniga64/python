5bb9f6e8953261ed59fb1f3c86b72a605e67d70a
Raul Zuniga <rzuniga64@gmail.com>
2015-02-03 01:41:19 -0600

    cleaned up file

--- cosc1336/src/lab/Lab3.py
+++ cosc1336/src/lab/Lab3.py
@@ -1,20 +1,22 @@
 def CToF(temp):
-    outputTemp = temp * 9 / 5 + 32
-    return outputTemp, 'F'
+    output_temp = temp * 9 / 5 + 32
+    return output_temp, 'F'
+
 
 def FToC(temp):
-    outputTemp = (temp -32) * 5/9
-    return outputTemp, 'C'
+    output_temp = (temp -32) * 5/9
+    return output_temp, 'C'
+
 
 def main():
-    inputTemp = float( input( 'Give temp: ' ) )
-    inputScale = input( 'Give scale: ')
-    inputScale = inputScale.upper()
-    print( inputTemp, inputScale)
-    if inputScale == "F":
-        outputTemp, outputScale = FToC(inputTemp)
+    input_temp = float(input('Give temp: '))
+    input_scale = input('Give scale: ')
+    input_scale = input_scale.upper()
+    print(input_temp, input_scale)
+    if input_scale == "F":
+        output_temp, output_scale = FToC(input_temp)
     else:
-        outputTemp, outputScale = CToF(inputTemp)
-    print( outputTemp, outputScale )
+        output_temp, output_scale = CToF(input_temp)
+    print(output_temp, output_scale)
 
 main()
