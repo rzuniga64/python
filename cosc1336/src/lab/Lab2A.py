9a762db9f30ce04729aad9f4faabe2036f0f878f
Raul Zuniga <rzuniga64@gmail.com>
2015-02-03 01:38:05 -0600

    cleaned up file

--- cosc1336/src/lab/Lab2A.py
+++ cosc1336/src/lab/Lab2A.py
@@ -1,20 +1,21 @@
-def howManyBillsOfThatType( change, bill ):
+def how_many_bills_of_that_type(change, bill):
     bills = change // bill
-    print( "There were ", bills, " of $", bill , "'s.", sep = "" )
+    print("There were ", bills, " of $", bill, "'s.", sep="")
     change = change % bill
     return change
-    
+
+
 def main():
-    cost = int( input( "Gimme the cost in $" ) )
-    tendered = int( input( "Gimme the amount rec'd. in $" ) )
+    cost = int(input("Gimme the cost in $"))
+    tendered = int(input("Gimme the amount rec'd. in $"))
     change = tendered - cost
-    print( "Change is $", change, ".", sep="" )
-    change = howManyBillsOfThatType( change, 500 )
-    change = howManyBillsOfThatType( change, 100 )
-    change = howManyBillsOfThatType( change, 50 )
-    change = howManyBillsOfThatType( change, 20 )
-    change = howManyBillsOfThatType( change, 10 )
-    change = howManyBillsOfThatType( change, 5 )
-    change = howManyBillsOfThatType( change, 1 )
+    print("Change is $", change, ".", sep="")
+    change = how_many_bills_of_that_type(change, 500)
+    change = how_many_bills_of_that_type(change, 100)
+    change = how_many_bills_of_that_type(change, 50)
+    change = how_many_bills_of_that_type(change, 20)
+    change = how_many_bills_of_that_type(change, 10)
+    change = how_many_bills_of_that_type(change, 5)
+    change = how_many_bills_of_that_type(change, 1)
 
 main()
