1ce3dfa81acc4183401198a5bfe53322d88a7eee
Raul Zuniga <rzuniga64@gmail.com>
2015-02-03 01:26:17 -0600

    cleaned up file

--- cosc1336/src/lab/lab8.py
+++ cosc1336/src/lab/lab8.py
@@ -1,27 +1,29 @@
-def getDigitsUsedInAddress( stNumber ):
-    theCounts = [ 0 ] * 10
-    for n in range( len( stNumber ) ):
-        digit = stNumber[n]
-        digitNo = int( digit )
-        theCounts[digitNo] += 1
-    return theCounts
+def get_digits_used_in_address(st_number):
+    the_counts = [0] * 10
+    for n in range(len(st_number)):
+        digit = st_number[n]
+        digit_no = int(digit)
+        the_counts[digit_no] += 1
+    return the_counts
 
-def showResults( theCounts ):
-    for digit in range( 10 ):
-        if theCounts[digit] > 0:
-            print( digit, "occurred", theCounts[digit] )
+
+def show_results(the_counts):
+    for digit in range(10):
+        if the_counts[digit] > 0:
+            print(digit, "occurred", the_counts[digit])
+
 
 def main():
     # Ask for street number.
-    startAddr = int(input("Give me a starting street number: " ))
-    endAddr = int(input("Give me a ending "))
-    addressString = ""
-    for address in range(startAddr, endAddr+1):   
-        addressString = addressString + str(address)
+    start_addr = int(input("Give me a starting street number: "))
+    end_addr = int(input("Give me a ending "))
+    address_string = ""
+    for address in range(start_addr, end_addr+1):
+        address_string += str(address)
 
     # Count digits used in that street number.
-    theCounts =  getDigitsUsedInAddress(addressString)
+    the_counts = get_digits_used_in_address(address_string)
     # Display digit use counts.
-    showResults( theCounts )
+    show_results(the_counts)
     
 main()
