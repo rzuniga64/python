ed14dfe77f30a1bbc6dd59803335764149f07ea4
Raul Zuniga <rzuniga64@gmail.com>
2015-02-03 01:46:11 -0600

    cleaned up file

--- cosc1336/src/lab/Lab5.py
+++ cosc1336/src/lab/Lab5.py
@@ -1,40 +1,43 @@
 import random
 
-def nameGesture( n ):
+
+def name_gesture(n):
     if n == 1:
         return "rock"
     elif n == 2:
         return "scissors"
     return "paper"
 
-def single( first, second ):
+
+def single(first, second):
     result = "PROBLEM"
-    firstGesture = nameGesture( first )
-    secondGesture = nameGesture( second )
+    first_gesture = name_gesture(first)
+    second_gesture = name_gesture(second)
 
-    if firstGesture == secondGesture:
+    if first_gesture == second_gesture:
         result = 'Neither player won. TIE!'
-    elif firstGesture == 'paper':
-        if secondGesture == 'rock':
+    elif first_gesture == 'paper':
+        if second_gesture == 'rock':
             result = '1st player won'
-        elif secondGesture == 'scissors':
+        elif second_gesture == 'scissors':
             result = '2nd player won'
-    elif firstGesture == 'scissors':
-        if secondGesture == 'rock':
+    elif first_gesture == 'scissors':
+        if second_gesture == 'rock':
             result = '2nd player won'
-        elif secondGesture == 'paper':
+        elif second_gesture == 'paper':
             result = '1st player won'
-    elif firstGesture == 'rock':
-        if secondGesture == 'scissors':
+    elif first_gesture == 'rock':
+        if second_gesture == 'scissors':
             result = '1st player won'
-        elif secondGesture == 'paper':
+        elif second_gesture == 'paper':
             result = '2nd player won'
 
-    print( firstGesture, secondGesture, result, '.' )
+    print(first_gesture, second_gesture, result, '.')
+
 
 def main():
-    for first in range( 1, 4 ):
-        for second in range( 1, 4):
-            single( first, second )
+    for first in range(1, 4):
+        for second in range(1, 4):
+            single(first, second)
 
 main()             
