ba6d0f50a2769ef5b33a36a33a6d8540419a3267
Raul Zuniga <rzuniga64@gmail.com>
2015-02-03 01:33:22 -0600

    cleaned up file

--- cosc1336/src/lab/lab10.py
+++ cosc1336/src/lab/lab10.py
@@ -1,48 +1,50 @@
 # Input: file name containing text to find unique words in
 # Output: list of unique words
 
-def ProcessDocument( filename ):
+
+def process_document(filename):
     try:
-        document = open("\\\RGC-FP0\\StudentsHome\\3051102\workspace\\python_workspace\\hw7\\src\\senators.txt", "r" )
+        document = open("\\\RGC-FP0\\StudentsHome\\3051102\workspace\\python_workspace\\hw7\\src\\senators.txt", "r")
     except:
-        print( "File " + filename + " did not open." )
+        print("File " + filename + " did not open.")
         document = open("senators.txt", "r")
 
     senators = []
-    demTotal = 0
-    repTotal = 0
-    demTotalYearsOfService = 0
-    repTotalYearsOfservice = 0
+    dem_total = 0
+    rep_total = 0
+    dem_total_years_of_service = 0
+    rep_total_years_of_service = 0
     
     for line in document:
         line = line.strip()
-        senatorFields = line.split()
-        senators.append(senatorFields)
-        yearsOfService = 2013 - int(senatorFields[-2])
-        if senatorFields[-3] == "(R)":
-            repTotal += 1
-            repTotalYearsOfservice += yearsOfService
-        if senatorFields[-3] == "(D)":   
-            demTotal += 1
-            demTotalYearsOfService += yearsOfService
+        senator_fields = line.split()
+        senators.append(senator_fields)
+        years_of_service = 2013 - int(senator_fields[-2])
+        if senator_fields[-3] == "(R)":
+            rep_total += 1
+            rep_total_years_of_service += years_of_service
+        if senator_fields[-3] == "(D)":
+            dem_total += 1
+            dem_total_years_of_service += years_of_service
             
-    print("The total number of republicans is ", repTotal)
-    print("The total number of democrats is ", demTotal)    
-    print("The total number of republican years of service is ", format(repTotalYearsOfservice, "d"))
-    print("The total number of democrat years of service is ", format(demTotalYearsOfService, "d"))        
+    print("The total number of republicans is ", rep_total)
+    print("The total number of democrats is ", dem_total)
+    print("The total number of republican years of service is ", format(rep_total_years_of_service, "d"))
+    print("The total number of democrat years of service is ", format(dem_total_years_of_service, "d"))
             
         #Watch for divide by zero         
-    aveRepYearsOfService = repTotalYearsOfservice/repTotal
-    aveDemYearsOfService = demTotalYearsOfService/demTotal
+    ave_rep_years_of_service = rep_total_years_of_service/rep_total
+    ave_dem_years_of_service = dem_total_years_of_service/dem_total
         
     document.close()
     
     return senators
 
+
 def main():
 
-    unique = ProcessDocument("\\\RGC-FP0\\StudentsHome\\3051102\workspace\\python_workspace\\hw7\\src\\senators.txt")
+    unique = process_document("\\\RGC-FP0\\StudentsHome\\3051102\workspace\\python_workspace\\hw7\\src\\senators.txt")
     unique.sort()
-    print( unique )
+    print(unique)
 
 main()
