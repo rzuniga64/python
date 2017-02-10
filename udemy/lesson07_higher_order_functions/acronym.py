c0a89f3d6168e5939336211005ebef3a0324cabd
Raul Zuniga <rzuniga64@gmail.com>
2017-02-10 03:30:41 -0600

    Update

--- udemy/lesson07_higher_order_functions/acronym.py
+++ udemy/lesson07_higher_order_functions/acronym.py
@@ -13,8 +13,12 @@ def acronym(words):
 
 
 def main():
-        words = ['in' ,'my', 'humble', 'opinion']
-        acro = acronym(words)
-        print(acro)
+    words = ['in', 'my', 'humble', 'opinion']
+    # acro = list(map(first, words))
+    # print(acro)
+    # acro = ''
+    # acro = acro.join(list(map(first, words))).upper()
+    acro = acronym(words)
+    print(acro)
     
 main()
