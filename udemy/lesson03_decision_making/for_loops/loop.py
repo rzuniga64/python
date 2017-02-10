302b556f11c52675e49b46cf2476086d2d0059a1
Raul Zuniga <rzuniga64@gmail.com>
2017-02-09 22:47:06 -0600

    Update

--- udemy/lesson03_decision_making/for_loops/loop.py
+++ udemy/lesson03_decision_making/for_loops/loop.py
@@ -1,5 +1,13 @@
-sum = 0
-numbers = range(1,11)
-for i in numbers:
-   sum += i
-print(sum)
+total = 0
+numbers = range(1, 11)
+for number in numbers:
+    total += number
+print("the total is " + str(total))
+
+sentence = "now is the time for all good people to come to the aid"
+count = 0
+for letter in sentence:
+    if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'u' \
+            or letter == 'o':
+        count += 1
+print("The number of vowels is " + str(count))
