22dac36493aec4fdb3cfa47c81fd5d3336454858
Raul Zuniga <rzuniga64@gmail.com>
2017-02-10 02:51:40 -0600

    Update

--- udemy/lesson07_higher_order_functions/recursive_factorial.py
+++ udemy/lesson07_higher_order_functions/recursive_factorial.py
@@ -2,10 +2,10 @@
 # 5! = 5 * 4!
 
 
-def fact(number):
-    if number <= 1:
+def fact(num):
+    if num <= 1:
         return 1
     else:
-        return number * fact(number-1)
+        return num * fact(num-1)
 number = 10
 print(str(number) + "! = " + str(fact(10)))
