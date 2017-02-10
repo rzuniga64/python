16134427822727209fba95195cb6e48e4e0f5b5e
Raul Zuniga <rzuniga64@gmail.com>
2017-02-10 03:16:48 -0600

    Update

--- udemy/lesson07_higher_order_functions/anonymous_function.py
+++ udemy/lesson07_higher_order_functions/anonymous_function.py
@@ -1,13 +1,14 @@
-# anonymous function
-# lambda form
+# anonymous function based on a lambda form
 
 # def square(number):
 #   return number * number
 
+# sq = square
+
 
 def main():
-    # square = lambda x: x*x
-    # print(square(2))
+    square = lambda x: x*x
+    print(square(2))
 
     numbers = [1, 2, 3, 4]
     numbers_sq = list(map(lambda x: x*x, numbers))
