9f661e6cee6891bce9164a9dcbf8897d3e7cc747
Raul Zuniga <rzuniga64@gmail.com>
2017-02-10 14:19:21 -0600

    add connect4 app - project 5

--- udemy/lesson08_module/newtontest.py
+++ udemy/lesson08_module/newtontest.py
@@ -1,11 +1,13 @@
-"""
-    Python modules are in the \Lib directory.
-"""
-import sys
-print(sys.path)
-import newton
+from newton import average, square, sqrt
+
+num1 = 199
+num2 = 78
+#print("The average is " + str(average(num1, num2)))
+#print(sqrt(9))
+avg = average(num1, num2)
+print("The average is " + str(avg))
+print("The square of the average " + str(square(avg)))
 
 print("Enter a number: ")
 number = int(input())
-# print(sqrt(number))
-# print(average(144, 9))
+print("the square root of " + str(number) + ' is ' + str(sqrt(number)))
