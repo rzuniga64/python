f972d0610000f5dad56cb2de5176e94ae4b4b5f9
Raul Zuniga <rzuniga64@gmail.com>
2017-02-10 03:13:16 -0600

    Update

--- udemy/lesson07_higher_order_functions/highorderfunctions.py
+++ udemy/lesson07_higher_order_functions/highorderfunctions.py
@@ -1,34 +1,46 @@
+# A higher order function is a function that uses another function as an
+# argument.
+# A function is an object in python. We can assign it to a variable.
+# We can pass a function as an argument to another function.
+
 # Let's look at three higher order functions:
 # map, filter and reduce
 # increases productivity and reduces errors
+
+
 def square(number):
-   return number * number
+    return number * number
+
 
 def even(number):
-   if number % 2 == 0:
-      return True
-   else:
-      return False
+    if number % 2 == 0:
+        return True
+    else:
+        return False
 
-def sum(x,y):
-   return x + y
+
+def total(x, y):
+    return x + y
+
 
 def main():
     # map
-    numbers = [1,2,3]
+    numbers = [1, 2, 3]
     print(numbers)
     numberssq = list(map(square, numbers))
     print(numberssq)
-    
-    numbers = list(range(1,11))
+
+    # filter
+    numbers = list(range(1, 11))
     print(numbers)
     evens = list(filter(even, numbers))
     print(evens)
-    
+
+    # reduce
     import functools
-    numbers = list(range(1,11))
+    numbers = list(range(1, 11))
     print(numbers)
-    sum = functools.reduce(sum, numbers)
+    sum = functools.reduce(total, numbers)
     print("The sum is of the range is " + str(sum))
 
 main()
