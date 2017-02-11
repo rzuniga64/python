58009cb053c9a550b1af2cb3b5a821456e9991eb
Raul Zuniga <rzuniga64@gmail.com>
2017-02-10 21:11:47 -0600

    add connect4 app - project 5

--- udemy/lesson04_listcomprehensions/list_of_lists.py
+++ udemy/lesson04_listcomprehensions/list_of_lists.py
@@ -1,7 +1,9 @@
-# Lists can contain any type of elements, including strings, nested lists and
-# functions. You can even mix different types within a list. The following works
-# on a list of strings and produces a list of lists. Each of the sublists
-# contains two strings and an integer.
+"""
+Lists can contain any type of elements, including strings, nested lists and
+functions. You can even mix different types within a list. The following works
+on a list of strings and produces a list of lists. Each of the sublists
+contains two strings and an integer.
+"""
 
 words = 'The quick brown fox jumps over the lazy dog'.split()
 print("Words =", words)
@@ -17,14 +19,16 @@ stuff = map(lambda w: [w.upper(), w.lower(), len(w)], words)
 for i in stuff:
     print(i)
 
-# The above example also demonstrates that you can do exactly the same thing
-# with map() and a lambda function. However, there are cases when you cannot
-# use map() and have to use a list comprehension instead, or vice versa. When
-# you can use both, then it is often preferable to use a list comprehension,
-# because this is more efficient and easier to read, most of the time.
+"""
+The above example also demonstrates that you can do exactly the same thing
+with map() and a lambda function. However, there are cases when you cannot
+use map() and have to use a list comprehension instead, or vice versa. When
+you can use both, then it is often preferable to use a list comprehension,
+because this is more efficient and easier to read, most of the time.
 
-# You cannot use list comprehensions when the construction rule is too
-# complicated to be expressed with "for" and "if" statements, or if the
-# construction rule can change dynamically at runtime. In this case, you better
-# use map() and / or filter() with an appropriate function. Of course, you can
-# combine that with list comprehensions.
+You cannot use list comprehensions when the construction rule is too
+complicated to be expressed with "for" and "if" statements, or if the
+construction rule can change dynamically at runtime. In this case, you better
+use map() and / or filter() with an appropriate function. Of course, you can
+combine that with list comprehensions.
+"""
--- udemy/lesson04_listcomprehensions/listcomp.py
+++ udemy/lesson04_listcomprehensions/listcomp.py
@@ -1,11 +1,14 @@
-# List comprehensions is a shortcut for a for loop.
-# Python supports a concept called "list comprehensions". It can be used to
-# construct lists in a very natural, easy way, like a mathematician is used to
-# do. The following are common ways to describe lists (or sets, or tuples, or
-# vectors) in mathematics.
-# S = {x² : x in {0 ... 9}}
-# V = (1, 2, 4, 8, ..., 2¹²)
-# M = {x | x in S and x even}
+"""
+List comprehensions is a shortcut for a for loop.
+Python supports a concept called "list comprehensions". It can be used to
+construct lists in a very natural, easy way, like a mathematician is used to do.
+The following are common ways to describe lists (or sets, or tuples, or vectors)
+in mathematics.
+
+S = {x² : x in {0 ... 9}}
+V = (1, 2, 4, 8, ..., 2¹²)
+M = {x | x in S and x even}
+"""
 
 S = [x**2 for x in range(10)]
 V = [2**i for i in range(13)]
