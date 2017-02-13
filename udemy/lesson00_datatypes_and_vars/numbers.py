e056e1d17cb76be341f5ca7b74c5734bfb00a14c
Raul Zuniga <rzuniga64@gmail.com>
2017-02-13 15:49:15 -0600

    Update

--- udemy/lesson00_datatypes_and_vars/numbers.py
+++ udemy/lesson00_datatypes_and_vars/numbers.py
@@ -1,8 +1,10 @@
-# Integers can be of any length, it is only limited by the memory available.
-# A floating point number is accurate up to 15 decimal places. Integer and floating points are separated by decimal
-# points. 1 is integer, 1.0 is floating point number.
-# Complex numbers are written in the form, x + yj, where x is the real part and y is the imaginary part.
-
+"""
+ Integers can be of any length, it is only limited by the memory available.
+ A floating point number is accurate up to 15 decimal places. Integer and
+ floating points are separated by decimal points. 1 is integer, 1.0 is floating
+ point number. Complex numbers are written in the form, x + yj, where x is the
+ real part and y is the imaginary part.
+"""
 a = 5
 print(a, "is of type", type(a))
 
