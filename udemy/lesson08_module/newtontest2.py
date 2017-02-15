3b9943077a2a27e538dc50561e7095fadfaffaf9
Raul Zuniga <rzuniga64@gmail.com>
2017-02-14 20:51:18 -0600

    Update

--- udemy/lesson08_module/newtontest2.py
+++
@@ -1,9 +0,0 @@
-from newton import average, square
-
-num1 = 199
-num2 = 78
-#print("The average is " + str(average(num1, num2)))
-#print(sqrt(9))
-avg = average(num1, num2)
-print("The average is " + str(avg))
-print("The square of the average " + str(square(avg)))
